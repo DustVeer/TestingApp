b0f81d2ad8728cb9cc9137aa8f1867928a01d56b
DustVeer <dveerdonk@gmail.com>
2025-11-03 12:52:56 +0100

    Same Test

--- src/test/java/TestingApp/TestingApp/TestingAppApplicationTests.java
+++ src/test/java/TestingApp/TestingApp/TestingAppApplicationTests.java
@@ -1,15 +1,12 @@
 package TestingApp.TestingApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=" +
+      "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+      "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 class TestingAppApplicationTests {
 
--- src/test/java/TestingApp/TestingApp/controllers/WeatherControllerTest.java
+++ src/test/java/TestingApp/TestingApp/controllers/WeatherControllerTest.java
@@ -1,7 +1,6 @@
 package TestingApp.TestingApp.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
